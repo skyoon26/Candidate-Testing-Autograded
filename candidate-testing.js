4c88db3ce8832ebd025821c4efac5e5bd93d03ab
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-01-20 23:23:25 -0600

    first attempt of part 2 graded assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,14 +6,24 @@ const input = require('readline-sync');
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
+let questionTwo = "True or false: 5 kilometer == 5000 meters? ";
+let questionThree = "(5 + 3)/2 * 10 = ? ";
+let questionFour = "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ";
+let questionFive = "What is the minimum crew size for the ISS? ";
+
 let correctAnswer = "Sally Ride";
+// let correctAnsTwo = "true";
+// let correctAnsThree = "40";
+// let correctAnsFour = "Trajectory";
+// let correctAnsFive = 3;
+
 let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [question, questionTwo, questionThree, questionFour, questionFive];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,17 +33,37 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  for (i = 0; i < questions.length; i++) {
+    candidateAnswer = input.question(questions[i]);
+    candidateAnswers.push(candidateAnswer);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if(candidateAnswer === correctAnswer){
-  console.log("Correct answer!");
-} else {
-  console.log("Incorrect answer.");
-}
+// if(candidateAnswer === correctAnswer){
+//   console.log("Correct answer!");
+// } else {
+//   console.log("Incorrect answer.");
+// }
+
+let feedback = `1) Your Answer: ${candidateAnswers[0]}
+Correct Answer: ${correctAnswers[0]}
+
+2) Your Answer: ${candidateAnswers[1]}
+Correct Answer: ${correctAnswers[1]}
+
+3) Your Answer: ${candidateAnswers[2]}
+Correct Answer: ${correctAnswers[2]}
+
+4) Your Answer: ${candidateAnswers[3]}
+Correct Answer: ${correctAnswers[3]}
+
+5) Your Answer: ${candidateAnswers[4]}
+Correct Answer: ${correctAnswers[4]}`
+
+console.log(feedback);
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
